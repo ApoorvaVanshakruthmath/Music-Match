27849161980cc24c6c7b94e3624b674cc0c4396b
Apoorva.Vanshakruthmath <117889978+ApoorvaVanshakruthmath@users.noreply.github.com>
2024-03-06 22:33:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './root'; // Import the renamed root file
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
